e1fff5612da164c2fc0523d437d929424e3511fc
ramchandragawade <raajgawade39@gmail.com>
2024-08-02 03:29:01 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import './App.css'
+const App = () => {
+  return <div>Hello World</div>;
+};
 
-function App() {
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+export default App;
