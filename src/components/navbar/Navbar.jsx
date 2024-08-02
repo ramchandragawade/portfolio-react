44bed0d19ebc2b28156777db63bb3fba9298519e
ramchandragawade <raajgawade39@gmail.com>
2024-08-02 06:08:15 +0530

    nav animate logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,35 @@
 import './navbar.scss';
+import { motion } from 'framer-motion';
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-      {/* {Sidebar} */}
-      <div className="wrapper">
-        <span className='logospan'>Ramchandra Gawade</span>
-        <div className="social">
-          <a href="#"><img src="/facebook.png" alt="facebook" /></a>
-          <a href="#"><img src="/insta.png" alt="instagram" /></a>
-          <a href="#"><img src="/linkedin.png" alt="linkedin" /></a>
-          <a href="#"><img src="/github.png" alt="github" /></a>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="navbar">
+			{/* {Sidebar} */}
+			<div className="wrapper">
+				<motion.span
+					className="logospan"
+					initial={{ opacity: 0, scale: 0.5 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 0.5 }}
+				>
+					Ramchandra Gawade
+				</motion.span>
+				<div className="social">
+					<a href="#">
+						<img src="/facebook.png" alt="facebook" />
+					</a>
+					<a href="#">
+						<img src="/insta.png" alt="instagram" />
+					</a>
+					<a href="#">
+						<img src="/linkedin.png" alt="linkedin" />
+					</a>
+					<a href="#">
+						<img src="/github.png" alt="github" />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
